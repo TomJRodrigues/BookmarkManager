2368e701d666d78c2edc21e7ff9f4d1f22ad8d29
Tom <tomjrodrigues@gmail.com>
2017-02-08 19:53:27 -0800

    added event handler for subject click

--- src/Subject.js
+++ src/Subject.js
@@ -3,14 +3,25 @@ import React, { Component } from 'react';
 export default class Subject extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      numberOfClicks: 0
+    }
   }
 
   // event handlers
 
+  onIncreaseCount() {
+    this.setState({
+      numberOfClicks: this.state.numberOfClicks + 1
+    });
+  }
+
   render() {
     return(
       <div>
-      	<h2>{this.props.items.subject}</h2>
+      	<h2 onClick={() => this.onIncreaseCount()}>{this.props.items.subject}</h2> 
+        Number of clicks: {this.state.numberOfClicks}
+
 
       	<ul>
       		{this.props.items.resources.map((resource) => {
